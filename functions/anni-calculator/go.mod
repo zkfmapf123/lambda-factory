13b53481307002daaae4e806ce78175418adfd77
zkfmapf123 <zkfmapf123@naver.com>
2024-06-17 22:22:58 +0900

    Update anni

--- functions/anni-calculator/go.mod
+++ functions/anni-calculator/go.mod
@@ -8,7 +8,12 @@ require (
 )
 
 require (
+	github.com/ashwanthkumar/slack-go-webhook v0.0.0-20200209025033-430dd4e66960 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/moul/http2curl v1.0.0 // indirect
+	github.com/parnurzeal/gorequest v0.3.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/net v0.26.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- functions/anni-calculator/main.go
+++ functions/anni-calculator/main.go
@@ -6,17 +6,20 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
+	"github.com/ashwanthkumar/slack-go-webhook"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 var (
 	TIME_LAYOUT = "2006-01-02"
 
-	START = os.Getenv("START_DAY")
-	BIRTH = os.Getenv("BIRTH_DAY")
+	START   = os.Getenv("START_DAY")
+	BIRTH   = os.Getenv("BIRTH_DAY")
+	WEBHOOK = "https://hooks.slack.com/services/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"
 )
 
 func HandleRequest(ctx context.Context, e json.RawMessage) (*int, error) {
@@ -44,12 +47,41 @@ func HandleRequest(ctx context.Context, e json.RawMessage) (*int, error) {
 	// 생일까지 몇일 남았는가?
 	leastForBirth := TimeSubBirthday(BIRTH, today)
 
-	fmt.Println("몇일 만났나요 ? ", todayFromBeginDay)
+	fmt.Println("몇일 만났나요 ? ", todayFromBeginDay+1)
 	fmt.Println("%99", is99)
 	fmt.Println("%100", is100)
 	fmt.Println("생일까지 남은 날짜 ? ", leastForBirth)
 
+	sendToSlackTemplate(strconv.Itoa(todayFromBeginDay+1), is99, is100, strconv.Itoa(leastForBirth))
 	return nil, nil
+}
+
+func sendToSlackTemplate(afterBeginDay string, is99 bool, is100 bool, afterBirthday string) {
+
+	attac := slack.Attachment{}
+	attac.AddField(slack.Field{Title: "--랑 몇일 만났니?", Value: afterBeginDay})
+
+	if is99 {
+		attac.AddField(slack.Field{Title: "*00일 하루 전 인가요?", Value: "Yes"})
+	}
+
+	if is100 {
+		attac.AddField(slack.Field{Title: "오늘은 00일 입니다", Value: "Yes"})
+	}
+
+	attac.AddField(slack.Field{Title: "-- 남은 생일 날", Value: afterBirthday})
+
+	now := GetToday()
+	p := slack.Payload{
+		Text:        now,
+		Channel:     "#chatbot-test",
+		Attachments: []slack.Attachment{attac},
+	}
+
+	err := slack.Send(WEBHOOK, "", p)
+	if err != nil {
+		panic(err)
+	}
 }
 
 // yyyy-mm-dd
